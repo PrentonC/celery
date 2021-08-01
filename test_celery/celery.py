be9f82509f339dbf69394c1c7fd3f7e76218c55b
PrentonC <prentonc@gmail.com>
2021-08-01 14:17:31 +0200

    removed password for rabbitmq

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 from celery import Celery
-app = Celery('test_celery', broker='amqp://admin:mypass@broker:5672',
+app = Celery('test_celery', broker='amqp://guest:guest@broker:5672',
              backend='rpc://', include=['test_celery.tasks'])
